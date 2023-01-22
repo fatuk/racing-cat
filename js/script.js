2c7b73b2474c3eec7205ec6c6bac993802ea6891
Dmitry Daschinsky <ddaschinsky@gmail.com>
2023-01-22 11:25:19 +0400

    Add turning and mouse

--- js/script.js
+++ js/script.js
@@ -11,16 +11,17 @@ class Cat {
   constructor() {
     this.el = document.querySelector('.cat');
     this.size = 100;
-    this.speed = 1;
-    this.maxSpeed = 20;
+    this.speed = 0;
+    this.maxSpeed = 10;
     this.x = 200;
     this.y = 200;
     this.targetX = 0;
     this.targetY = 0;
-    this.acceleration = 0.2;
-    this.friction = this.acceleration * 2;
+    this.acceleration = 0.05;
+    this.friction = this.acceleration * 3;
     this.isMoving = false;
     this.angle = 0;
+    this.maxAngleTweak = 3;
   }
 
   render() {
@@ -55,6 +56,9 @@ class Cat {
 
   slowDown() {
     this.isMoving = false;
+    // Mouse is caught
+    mouse.style.display = 'none';
+
     this.speed -= this.friction;
 
     if (this.speed < 0) {
@@ -65,7 +69,20 @@ class Cat {
     this.y = this.y + this.speed * sin(this.angle);
   }
 
-  move() {
+  move() {  
+    const angle = getAngle({x: this.x, y: this.y}, {x: this.targetX, y: this.targetY});
+    const newAngleDeg = radToDeg(angle);
+    
+    if (this.isMoving) {
+      if (this.speed == 0) {
+        this.angle = newAngleDeg;
+      } 
+      else {
+        // update angle
+        this.angle = getNewAngle(this.angle, newAngleDeg, this.maxAngleTweak);
+      }
+    }
+
     if (Math.hypot(this.targetX - this.x, this.targetY - this.y) > this.speed && this.isMoving) {
       this.speedUp();
     } else {
@@ -77,16 +94,27 @@ class Cat {
 }
 
 const cat = new Cat();
+const mouse = document.getElementsByClassName('mouse')[0];
+
+document.addEventListener('keyup', (event) => {
+  if (event.key == " " ||
+      event.code == "Space") {
+        cat.speed = 0;
+        cat.isMoving = false;
+  }
+});
 
 document.addEventListener('click', (event) => {
   const { clientX, clientY } = event;
 
-  if (cat.isMoving) {
-    return
-  }
+  // if (cat.isMoving) {
+  //   return
+  // }
 
-  const angle = getAngle({x: cat.x, y: cat.y}, {x: clientX, y: clientY});
-  cat.angle = radToDeg(angle);
+  mouse.style.display = 'block';
+  mouse.style.left = `${clientX}px`;
+  mouse.style.top = `${clientY}px`;
+
   cat.isMoving = true;
   cat.targetX = clientX;
   cat.targetY = clientY;
@@ -106,4 +134,32 @@ function cos(deg) {
 
 function radToDeg(rad) {
   return rad * 180 / Math.PI;
+}
+
+function getNewAngle(current, target, step) {
+  let inversion = 1;
+  let result = current;
+  let delta = Math.abs(target - current);
+  if (delta > 180) {
+    delta = 360 - delta;
+    inversion = -1;
+  }
+
+  if (delta <= step) {
+    result = target;
+  }
+  else {
+    result += target > current ? 
+      step * inversion : - step * inversion;   
+  }
+
+  // We operate with angle in range -180 : 180.
+  if (result < -180) {
+    result += 360;
+  }
+  else if (result > 180) {
+    result -= 360;
+  }
+
+  return result;
 }
